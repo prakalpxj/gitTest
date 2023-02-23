0d44a58c92d4da7866c5e55df134f46bdf5bcaf4
prakalpxj <prakalp.jain.05@gmail.com>
2023-02-23 23:05:20 +0530

    this is the first commit to create master branch :wq

---
+++ src/main/java/com/example/testGit/TestGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.testGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitApplication.class, args);
+	}
+
+}
